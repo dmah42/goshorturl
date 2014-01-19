b44616c96659894a8c5f58250dfea5cbc0005f1c
Dominic Hamon <dma+github@stripysock.com>
2014-01-18 21:31:37 -0800

    Initial version

---
+++ src/goshorturl/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+var (
+	dbConn *sql.DB = nil
+	db             = flag.String("db", "", "the db")
+	user           = flag.String("user", "", "the db user")
+	pwd            = flag.String("pwd", "", "the db password")
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	shorturl := mux.Vars(r)["shorturl"]
+
+	// lookup shorturl
+	var longurl string
+	var access_count int
+	err := dbConn.QueryRow("select long,access_count from url where short=?", shorturl).Scan(&longurl, &access_count)
+
+	// TODO: update accessed and access_count
+
+	switch {
+	case err == sql.ErrNoRows:
+		http.NotFound(w, r)
+
+	case err != nil:
+		log.Fatal(err)
+
+	default:
+		access_count++
+		http.Redirect(w, r, longurl, 200)
+	}
+}
+
+func main() {
+	dbConn, err := sql.Open("mysql", *user+":"+*pwd+"@/"+*db)
+	if err != nil {
+		log.Fatal("Failed to open sql dbConnection")
+	}
+	defer dbConn.Close()
+
+	r := mux.NewRouter()
+	// TODO: dashboard with metrics
+	r.HandleFunc("/{shorturl}", Handler).Methods("GET")
+	http.Handle("/", r)
+}
